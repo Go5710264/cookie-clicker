3ada1e4b4b3d8b39688fe16542f4f26794e2b024
Dima Shirkov <dimashirkov@list.ru>
2022-05-03 06:51:21 +0300

    Dak1 draft v2

--- task.js
+++ task.js
@@ -2,6 +2,20 @@ let cookie = document.getElementById('cookie');
 let clickerCounter = document.getElementById('clicker__counter');
 clickerCounter.textContent = 0;
 
-cookie.onclick = function() {
+function imageEnlargement() {
+    if (cookie.width = '200') {
+        cookie.width = '250';
+        setTimeout(imageReduction, 100);
+    };
+};
+
+function imageReduction() {
+    if (cookie.width = '250') {
+        cookie.width = '200'};
+};
+
+cookie.onclick = function () { 
     return ++clickerCounter.textContent;
 };
+
+cookie.addEventListener('click', imageEnlargement);
